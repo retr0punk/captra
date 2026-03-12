17ad7a990bc0a55f57ff34d2389f7421654a037d
retr0punk <me@retr0punk.xyz>
2026-03-12 19:21:22 +0530

    fix: add 'dist' to .gitignore

--- packages/shared/dist/constants/http-error-code.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const httpErrorCode: {
-    readonly 400: "invalid_request_body";
-    readonly 422: "validation_error";
-    readonly 500: "internal_server_error";
-};
-//# sourceMappingURL=http-error-code.d.ts.map
